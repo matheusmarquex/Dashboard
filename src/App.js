5b17446bfedf47632923493992f7a959f3c90915
matheusmarquex <mmarxdev@gmail.com>
2021-06-01 00:37:28 -0300

    Feito Sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {useState} from 'react'
 import './App.css';   
+import Sidebar from './components/sidebar/Sidebar';
 
-function App() {
+
+const App = () => {
+  
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  
+  const openSidebar = () => {
+    setSidebarOpen(true)
+  }
+
+  const closeSidebar = () => {
+    setSidebarOpen(false)
+  }
+  
   return (
-    <div className="App">
-      <h1>Hello Marx :)</h1>
+    <div className="container">
+      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar}/>
     </div>
   );
 }
